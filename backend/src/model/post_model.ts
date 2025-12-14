e45bdf545d9bef95204c113db84b4ae5f79e31f5
BharatKhatwani <bharatkhatwani06@gmail.com>
2025-12-14 08:56:13 +0000

    schema push

---
+++ backend/src/model/post_model.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IPost extends Document {
+  userId: mongoose.Types.ObjectId;
+  imageUrl: string;
+  caption: string;
+}
+
+const PostSchema = new Schema<IPost>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    caption: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model<IPost>("Post", PostSchema);
+
+export default Post;
